f14376205611ada98dada4647112abdb1f33807c
Alex <eddings.sharanna@gmail.com>
2022-11-25 10:54:45 -0500

    updated input types for New.jsx

--- views/New.jsx
+++ views/New.jsx
@@ -4,11 +4,14 @@ class New extends React.Component {
     render() {
         return (
             <div>
-                <form action="/logs" method="post">
-                    <input type="text" name="title" /><br />
-                    <input type="textarea" name="entry" /><br />
-                    <input type="checkbox" name="shipIsBroken"/><br />
-                    <input type="submit" name="" />
+                <form action="/logs" method="POST">
+                    Title: <input type="text" name="title"  /><br />
+                    <br />
+                    Entry: <input type="textarea" name="entry" /><br />
+                    <br />
+                    Ship is Broken: <input type="checkbox" name="shipIsBroken"/><br />
+                    <br />
+                    <input type="submit" name="Submit" />
                 </form>
             </div>
         )
